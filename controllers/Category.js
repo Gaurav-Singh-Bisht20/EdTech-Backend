fc420bbd3999c3b66eac2dc088b8a64693a4ac4a
gaurav bisht <gauravengg.bisht@gmail.com>
2024-01-11 15:17:02 +0530

    category contoller and some basic routes added

---
+++ controllers/Category.js
@@ -0,0 +1,90 @@
+const Category = require("../models/Category");
+
+exports.createCategory = async (req, res) => {
+	try {
+		const { name, description } = req.body;
+		if (!name) {
+			return res
+				.status(400)
+				.json({ success: false, message: "All fields are required" });
+		}
+		const CategorysDetails = await Category.create({
+			name: name,
+			description: description,
+		});
+		console.log(CategorysDetails);
+		return res.status(200).json({
+			success: true,
+			message: "Categorys Created Successfully",
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: true,
+			message: error.message,
+		});
+	}
+};
+
+exports.showAllCategories = async (req, res) => {
+	try {
+		const allCategorys = await Category.find(
+			{},
+			{ name: true, description: true }
+		);
+		res.status(200).json({
+			success: true,
+			data: allCategorys,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
+
+//categoryPageDetails 
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+            //get categoryId
+            const {categoryId} = req.body;
+            //get courses for specified categoryId
+            const selectedCategory = await Category.findById(categoryId)
+                                            .populate("courses")
+                                            .exec();
+            //validation
+            if(!selectedCategory) {
+                return res.status(404).json({
+                    success:false,
+                    message:'Data Not Found',
+                });
+            }
+            //get coursesfor different categories
+            const differentCategories = await Category.find({
+                                         _id: {$ne: categoryId},
+                                         })
+                                         .populate("courses")
+                                         .exec();
+
+            //get top 10 selling courses
+            //HW - write it on your own
+
+            //return response
+            return res.status(200).json({
+                success:true,
+                data: {
+                    selectedCategory,
+                    differentCategories,
+                },
+            });
+
+    }
+    catch(error ) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
