f7266c4301c9ab7288fb2ae97e93037517f21e0b
Gaurav Singh (Bisht) <115692205+Gaurav-Singh-Bisht20@users.noreply.github.com>
2024-06-03 11:47:43 +0530

    Update contactFormRes.js

--- mail/templates/contactFormRes.js
+++ mail/templates/contactFormRes.js
@@ -75,7 +75,7 @@ exports.contactUsEmail = (
   <body>
       <div class="container">
           <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                  src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+                  src="https://res.cloudinary.com/dzfxz69u1/image/upload/v1717395196/bormj5yso3iu2fyxyxch.png"></a>
           <div class="message">Contact Form Confirmation</div>
           <div class="body">
               <p>Dear ${firstname} ${lastname},</p>
